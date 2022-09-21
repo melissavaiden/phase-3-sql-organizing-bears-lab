c4aaa2c4aab67b1ac8be085171928ca4fd1da16e
Melissa Vaiden <melissavaiden@gmail.com>
2022-09-21 17:25:15 -0400

    commit tree c1528d79f7dad6a97f09a0fe35e5cf240df835f2

---
+++ lib/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO bears (name, sex)
+VALUES ('Mr. Chocolate', 'M'), ('Rowdy', 'F'), ('Tabitha', 'M'), ('Sergeant Brown', 'F'), ('Melissa', 'M'), ('Grinch', 'F'), ('Wendy', 'M'), (NULL, 'M');
